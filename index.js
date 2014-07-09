ee850d01dcd1c007fb33002f09bccb008bdef11d
Eric Gjertsen <ericgj72@gmail.com>
2014-07-08 23:06:37 -0400

    a deadend but maybe on the right track

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ var dispatch = require('d3-dispatch')
 module.exports = function(){
   
   var fieldsets = []
+    , legends = []
+    , inputs = []
     , data = {}
-    , dispatcher = dispatch('input')
+    , dispatcher = dispatch('input','submit','cancel','reset')
+    , submit
+    , cancel
     , formclass
 
   render.classed = function(_){
@@ -15,14 +19,26 @@ module.exports = function(){
   }
 
   render.fieldset = function(){
-    fieldsets.push([ null, [].slice.call(arguments,0) ]);
+    fieldsets.push([].slice.call(arguments,0));
     return this;
   }
 
   render.legend = function(_){
     if (fieldsets.length == 0) return this;
-    fieldsets[fieldsets.length-1][0] = _;
+    legends[fieldsets.length-1] = _;
     return this;
+  }
+
+  render.submit = function(_){
+    submit = _; return this;
+  }
+
+  render.cancel = function(_){
+    cancel = _; return this;
+  }
+
+  render.input = function(_){
+    inputs.push(_); return this;
   }
 
   render.data = function(_){
@@ -35,27 +51,199 @@ module.exports = function(){
     return this;
   }
 
-  function render(selection){
-    var form = selection.selectAll('form').data([data])
-    var enter = 
-      form.enter()
-        .append('form')
-          .classed(formclass || "", !!formclass)
+  render.bindReset = function(fn,name){
+    if (('object' == typeof fn) && fn.reset) fn = fn.reset.bind(fn);
+    dispatcher.on('reset' + (name ? '.'+name : ''), fn);
+    return this;
+  }
 
-    fieldsets.forEach( function(fs){
-      var legend = fs[0]
-        , fields = fs[1]
-      var fieldset = enter.append('fieldset');
-      if (legend) fieldset.append('legend').text(legend);
-      fields.forEach( function(field){
-        field.dispatch(dispatcher);         // set dispatcher
-        fieldset.call( field.enter );       // enter
-        d3.select(fieldset.node()).call( field );  // update
-      })
-    })
+  render.bindSubmit = function(fn,name){
+    if (('object' == typeof fn) && fn.save) fn = fn.save.bind(fn);
+    dispatcher.on('submit' + (name ? '.'+name : ''), fn);
+    return this;
+  }
+
+
+  function render(selection){
     
+    var form = renderForm(selection);
+
+    renderFields( renderFieldsets(form), fieldsets );
+    
+    renderFields( renderInputs(form), [inputs] );
+
+    renderCancel( form );
+
+    renderSubmit( form );
+
+    form.on('submit', dispatchSubmit);
+  }
+
+  function renderForm(selection){
+    
+    // form
+    var form = selection.selectAll('form').data([data]);
+    form.enter()
+          .append('form')
+            .classed(formclass || "", !!formclass);
+    
+    form.exit().remove();
+    return form;
+
+  }
+
+  function renderInputs(selection){
+
+    // form input fields
+    var fielddata = inputs.map( function(field,j){
+      return { fieldset: 0, field: j, data: data };
+    })
+
+    var section = form.selectAll('section.fields').data([fielddata]);
+    section.enter()
+          .append('section').classed('fields',true);
+    
+    section.exit().remove();
+    return section;
+  }
+
+  function renderFieldsets(selection){
+
+    // fieldsets
+    var fielddata = fieldsets.map( function(fields,i){ 
+      return fields.map( function(field,j){ 
+        return { fieldset: i, field: j, data: data }; 
+      }); 
+    });
+
+    var fsets = selection.selectAll('fieldset').data(fielddata);
+    fsets.enter()
+      .append('fieldset')
+        .append('legend')
+          .text(function(d,i){ return legends[i];} );
+    
+    fsets.exit().remove();
+
+    return fsets;
+  }
+
+  function renderFields(selection, fields){
+
+    // field sections -- assumes data has been bound to selection
+    var sections = selection.selectAll('section.field').data(function(d){ return d; });
+    var enter = 
+      sections.enter()
+        .append('section').classed('field',true)
+
+    sections.each( function(d){ 
+      var section = d3.select(this);
+      var field = fields[d.fieldset][d.field];
+      section.datum(d.data);
+      buildField(field, enter, section); 
+    });
+
+    sections.exit().remove();
+
+    return sections;
   }
   
+  function renderCancel(selection){
+    if ('string' == typeof cancel) cancel = inputCancel(cancel);
+    return renderSection(selection, 'cancel', cancel);
+  }
+
+  function renderSubmit(selection){
+    if ('string' == typeof submit) submit = inputSubmit(submit);
+    return renderSection(selection, 'submit', submit);
+  }
+
+  function renderSection(selection, classed, field){
+    var sectiondata = (undefined === field ? [] : [data]);
+    var section = select.selectAll('section.'+classed).data(sectiondata);
+    var enter = 
+      section.enter()
+        .append('section').classed(classed,true)
+
+    if (field) buildField(field, enter, section); 
+
+    section.exit().remove();
+    return section;
+  }
+
+  function enterField(field, enter){
+    if (field.dispatch) field.dispatch(dispatcher);
+    enter.call( field.enter );
+  }
+
+  function updateField(field,update){
+    update.call( field );
+  }
+
+  function buildField(field, enter, update){
+    if (field.dispatch) field.dispatch(dispatcher);
+    enter.call( field.enter );
+    update.call( field );
+  }
+
+  function dispatchSubmit(d,i){
+    d3.event.preventDefault();  // disable browser submit
+    dispatcher.submit(d,i);
+  }
+
   rebind(render, dispatcher, 'on');
   return render;
 }
+
+
+function inputSubmit(name){
+
+  var labeltext = name
+
+  render.label = function(_){
+    labeltext = _; return this;
+  }
+
+  render.enter = function(enter){
+    enter.append('input').attr('type','submit').attr('name',name);
+  }
+
+  function render(selection){
+    var btn = selection.select('input[name='+name+']');
+    btn.attr('value',labeltext);
+  }
+
+  return render;
+}
+
+function inputCancel(name){
+
+  var labeltext = name
+    , dispatcher
+
+  render.dispatcher = function(_){
+    dispatcher = _; return this;
+  }
+
+  render.label = function(_){
+    labeltext = _; return this;
+  }
+
+  render.enter = function(enter){
+    enter.append('input').attr('type','button').attr('name',name);
+  }
+
+  function render(form){
+    var btn = form.select('input[name='+name+']');
+    btn.attr('value',labeltext);
+    if (dispatcher){
+      btn.on('click', dispatchCancel);
+    }
+  }
+
+  function dispatchCancel(){
+    dispatcher.cancel();
+  }
+
+  return render;
+}
+
